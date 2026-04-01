cdfcd8d7bc245d6c7c797e798c84e7a3637e023c
Kokkinis99 <gkokkinis@protonmail.com>
2026-04-01 23:34:56 +0300

    feat: remove fading from subdomain let it be handled completely by main website

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { afterNextRender, Component, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,17 +9,4 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('MoodTunePortfolio');
-
-  constructor() {
-    afterNextRender(() => {
-      const overlay = document.getElementById('mood-overlay');
-      if (overlay) {
-        overlay.classList.add('fade-out');
-        overlay.addEventListener('transitionend', () => {
-          overlay.remove();
-          document.documentElement.style.removeProperty('background');
-        }, { once: true });
-      }
-    });
-  }
 }
